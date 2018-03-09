69d0e4fd7dca4c0699350510f09f0aceb912ff63
KjellConnelly <kjellapps@gmail.com>
2018-03-09 12:10:44 -0800

    good

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { NativeModules } from 'react-native'
 const { RNReactNativeSharedGroupPreferences } = NativeModules
 
 export default class SharedGroupPreferences {
-  static async getItem(key) {
+  static async getItem(key, appGroup) {
     // RNReactNativeSharedGroupPreferences
     let item = 'You got an item'
     return new Promise(resolve=>{
@@ -10,7 +10,7 @@ export default class SharedGroupPreferences {
     })
   }
 
-  static async setItem(key, value) {
+  static async setItem(key, value, appGroup) {
     let error = null
 
     return new Promise(resolve=>{
--- ios/RNReactNativeSharedGroupPreferences.m
+++ ios/RNReactNativeSharedGroupPreferences.m
@@ -3,11 +3,39 @@
 
 @implementation RNReactNativeSharedGroupPreferences
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+  NSUserDefaults *mySharedDefaults;
+  NSString *appGroupName = @"";
+
+- (dispatch_queue_t)methodQueue {
+  return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
 
+  RCT_EXPORT_METHOD(getItem: (NSString *)key: (NSString *)appGroup: (RCTResponseSenderBlock) callback) {
+    if (![appGroup isEqualToString:appGroupName]) {
+      mySharedDefaults = [[NSUserDefaults alloc] initWithSuiteName:appGroup];
+    }
+    if (mySharedDefaults == nil) {
+      // error code 0 == no user defaults with that suite name available
+      callback(@[@0]);
+      return;
+    }
+
+    callback(@[[NSNull null], [mySharedDefaults valueForKey:key]]);
+  }
+
+  RCT_EXPORT_METHOD(setItem: (NSString *)key: (NSString *)value: (NSString *)appGroup: (RCTResponseSenderBlock) callback) {
+    if (![appGroup isEqualToString:appGroupName]) {
+      mySharedDefaults = [[NSUserDefaults alloc] initWithSuiteName:appGroup];
+    }
+    if (mySharedDefaults == nil) {
+      // error code 0 == no user defaults with that suite name available
+      callback(@[@0]);
+      return;
+    }
+
+    [mySharedDefaults setValue:value forKey:key];
+    callback(@[[NSNull null]]);
+  }
+
 @end
-  
