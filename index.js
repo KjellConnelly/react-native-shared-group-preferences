07037b78779b5abc9465d2204192c502713e0c21
KjellConnelly <kjellapps@gmail.com>
2018-03-09 14:54:16 -0800

    Finished iOS version

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import { NativeModules } from 'react-native'
+import { NativeModules, Platform } from 'react-native'
 const { RNReactNativeSharedGroupPreferences } = NativeModules
 
 export default class SharedGroupPreferences {
 
   static async getItem(key, appGroup) {
     return new Promise((resolve, reject)=>{
+      if ((Platform.OS != 'ios') && (Platform.OS != 'android')) {
+        reject(Platform.OS)
+      }
+
       RNReactNativeSharedGroupPreferences.getItem(key, appGroup, (error, item)=>{
         if (error != null) {
           reject(error)
@@ -17,6 +21,10 @@ export default class SharedGroupPreferences {
 
   static async setItem(key, value, appGroup) {
     return new Promise((resolve, reject)=>{
+      if ((Platform.OS != 'ios') && (Platform.OS != 'android')) {
+        reject(Platform.OS)
+      }
+
       RNReactNativeSharedGroupPreferences.setItem(key, JSON.stringify(value), appGroup, error=>{
         if (error != null) {
           reject(error)
