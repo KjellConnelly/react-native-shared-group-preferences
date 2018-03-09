7fcd87c328f5bc789c228e6b3ae7454902f608b5
KjellConnelly <kjellapps@gmail.com>
2018-03-09 12:19:30 -0800

    good

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ import { NativeModules } from 'react-native'
 const { RNReactNativeSharedGroupPreferences } = NativeModules
 
 export default class SharedGroupPreferences {
+
   static async getItem(key, appGroup) {
     let response = RNReactNativeSharedGroupPreferences.getItem(key, appGroup)
-
-
     return new Promise((resolve, reject)=>{
       alert(response)
       resolve("getItem")
@@ -14,10 +13,6 @@ export default class SharedGroupPreferences {
 
   static async setItem(key, value, appGroup) {
     let response = RNReactNativeSharedGroupPreferences.setItem(key, value, appGroup)
-
-
-    let error = null
-
     return new Promise((resolve, reject)=>{
       alert(response)
       resolve("setItem")
