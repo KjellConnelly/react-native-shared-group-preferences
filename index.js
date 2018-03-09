48a2cd62757d7c3c2890980e7a6b09c801a21e99
KjellConnelly <kjellapps@gmail.com>
2018-03-09 12:27:25 -0800

    good

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ export default class SharedGroupPreferences {
   static async getItem(key, appGroup) {
     let response = RNReactNativeSharedGroupPreferences.getItem(key, appGroup)
     return new Promise((resolve, reject)=>{
-      alert(response)
       resolve("getItem")
     })
   }
@@ -14,7 +13,6 @@ export default class SharedGroupPreferences {
   static async setItem(key, value, appGroup) {
     let response = RNReactNativeSharedGroupPreferences.setItem(key, value, appGroup)
     return new Promise((resolve, reject)=>{
-      alert(response)
       resolve("setItem")
     })
   }
